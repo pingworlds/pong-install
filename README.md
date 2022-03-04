6a453d513658cc410901485f2d049c817dc21f4d
Light Waiter <ortroute@gmail.com>
2022-03-04 21:24:02 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pong-install
+
+pong-go installation script
+
+    curl -o- -L https://github.com/pingworlds/pong-install//releases/latest/download/install.sh | bash
--- install.sh
+++ install.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-echo  'install pong'
+echo 'install pong'
 sys_os="unsupported"
 sys_arch="unknown"
 sys_arm=""
 
 app_name="pong"
-app_ver="0.9.1"
+# app_ver="0.9.1"
 app_service="pong.service"
 app_file_ext=".tar.gz"
 app_site="https://github.com/pingworlds"
-
-https://github.com/pingworlds/pong/releases/latest/download/package.zip.
-
 
 app_bin="$app_name"
 app_path="/usr/local/${app_name}"
 app_source="${app_site}/${app_name}"
 
-app_source="${app_site}/pong-install/release/download"
-app_release="${app_site}/${app_name}/releases/download"
-# app_release="${app_site}/${app_name}/"
-echo  "os type:$OSTYPE" 
-check_sys(){ 
-    unamem="$(uname -m)"
-    echo  "usname:$unamem"    
+app_source="${app_site}/pong"
+app_release="${app_site}/${app_name}/releases/latest/download"
+registered=0
+echo "os type:$OSTYPE"
+check_sys() {
+	unamem="$(uname -m)"
+	echo "usname:$unamem"
 	if [[ $unamem == *aarch64* ]]; then
 		sys_arch="arm64"
 	elif [[ $unamem == *x86_64* ]]; then
 		sys_arch="amd64"
 	elif [[ $unamem == *86* ]]; then
-		sys_arch="i386" 
+		sys_arch="i386"
 	elif [[ $unamem == *armv5* ]]; then
 		sys_arch="arm"
 		sys_arm="v5"
@@ -40,59 +37,148 @@ check_sys(){
 		sys_arch="arm"
 		sys_arm="v7"
 	else
-		echo "Aborted, no release version for architecture: $unamem."  
-        echo "please try compile and install from source code, $app_source"
+		echo "Aborted, no release version for architecture: $unamem."
+		echo "please try compile and install from source code, $app_source"
 		exit 1
 	fi
 
-     
-
 	unameu="$(tr '[:lower:]' '[:upper:]' <<<$(uname))"
 	if [[ ${unameu} == *DARWIN* ]]; then
-		sys_os="darwin"	  
+		sys_os="darwin"
 	elif [[ ${unameu} == *LINUX* ]]; then
-		sys_os="linux" 
-    elif [[ ${unameu} == *FREEBSD* ]]; then
+		sys_os="linux"
+	elif [[ ${unameu} == *FREEBSD* ]]; then
 		sys_os="freebsd"
 	elif [[ ${unameu} == *OPENBSD* ]]; then
 		sys_os="openbsd"
-	elif [[ ${unameu} == *WIN* ]]; then	 
-		sys_os="windows" 
-        app_file_ext=".zip"
-        app_bin="${app_name}.exe"
+	elif [[ ${unameu} == *WIN* ]]; then
+		sys_os="windows"
+		app_file_ext=".zip"
+		app_bin="${app_name}.exe"
 	else
 		echo "Aborted, no release version for current os: $unameu."
-        echo "please try compile and install from source code, $app_source"
+		echo "please try compile and install from source code, $app_source"
 		exit 1
 	fi
 }
 
+download_file() {
+	sudo systemctl stop pong
+	app_file_name="${app_name}_${sys_os}_${sys_arch}${sys_arm}"
+	dl_url="${app_release}/${app_file_name}${app_file_ext}"
+
+	echo "--------------current os:$sys_os  arch:$sys_arch"
+	echo "--------------${app_name} install path:$app_path"
+
+	# echo "--------------pong.service :$app_service"
+	echo "--------------pong :$dl_url"
+
+	if ! ls "$app_path" >/dev/null 2>&1; then
+		echo "--------------mk dir :$app_path"
+		mkdir $app_path
+	fi
+
+	curl -# -sL -O $dl_url
+	tar -zxf ${app_file_name}${app_file_ext} -C ${app_path}
+	rm -rf ${app_file_name}${app_file_ext}
+
+	chmod 700 /usr/local/pong/pong
+
+	if ! ls "/usr/local/${app_name}/${app_bin}" >/dev/null 2>&1; then
+		ln -s "/usr/local/${app_name}/${app_bin}" "/usr/bin/${app_bin}"
+	fi
+}
+
+reg_service() {
+	if ! ls "/usr/lib/systemd/system/pong.service" >/dev/null 2>&1; then
+		echo "registering pong service"
+		cat <<EOT >>/usr/lib/systemd/system/pong.service
+[Unit]
+Description=pong service
+Documentation=https://github.com/pingworlds/pong
+After=network.target network-online.target nss-lookup.target
+
+[Service]
+Type=simple
+StandardError=journal
+ExecStart=/usr/bin/pong -d /usr/local/pong
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+RestartSec=3s
+
+[Install]
+WantedBy=multi-user.target
+EOT
+	fi
+
+	if ! ls "/usr/local/pong/remote.json" >/dev/null 2>&1; then
+		cat <<EOT >>/usr/local/pong/remote.json
+{ 
+    "listens": [           
+        {
+            "transport": "h2c",
+            "host": ":21984",
+            "protocol": "pong",          
+            "clients": [
+                "d931e571-c9d2-4527-9223-9ef1cdeaf4b2"
+            ],            
+            "disabled": false
+        },
+        {
+            "transport": "http",
+            "host": ":21985",
+            "protocol": "pong",          
+            "clients": [
+                "d931e571-c9d2-4527-9223-9ef1cdeaf4b2"
+            ],            
+            "disabled": false
+        },
+        {
+            "transport": "ws",
+            "host": ":21986",           
+            "protocol": "pong",
+            "clients": [
+                "d931e571-c9d2-4527-9223-9ef1cdeaf4b2"
+            ],
+            "disabled": false
+        },
+		{
+            "transport": "h2c",
+            "host": ":21987",           
+            "protocol": "vless",
+            "clients": [
+                "d931e571-c9d2-4527-9223-9ef1cdeaf4b2"
+            ],
+            "disabled": false
+        },
+		{
+            "transport": "h2c",
+            "host": ":21988",           
+            "protocol": "qsocks",          
+            "disabled": false
+        },
+		{
+            "transport": "h2c",
+            "host": ":21989",           
+            "protocol": "ss",          
+            "disabled": false
+        }
+    ]
+}
+EOT
+	fi
+
+	sudo systemctl enable pong
+	systemctl daemon-reload
+
+	if ! ls "/etc/systemd/system/multi-user.target.wants/${app_service}" >/dev/null 2>&1; then
+		ln -s "/usr/lib/systemd/system/${app_service}" "/etc/systemd/system/multi-user.target.wants/${app_service}"
+	fi
+	echo "successfully registered pong service."
+}
+
 check_sys
- 
-echo  "current os:$sys_os arch:$sys_arch"
-echo  "${app_name} install path:$app_path  bin:${app_bin}  file_ext:$app_file_ext" 
+download_file
+reg_service
 
-app_file_name="${app_name}_${app_ver}_${sys_os}_${sys_arch}${sys_arm}"
-# dl_url="${app_release}/v${app_ver}/${app_file_name}${app_file_ext}"
-dl_url="${app_release}/${app_file_name}${app_file_ext}"
-# app_release
-echo "download from:$dl_url"
-
-if ! ls /usr/local/${app_name} >/dev/null 2>&1; then
-    mkdir $app_path
-	chmod -x $app_path
-
-	curl -# -sL -O $dl_url   | tar -zxf ${app_file_name}${app_file_ext} -C ${app_path}
-fi 
- 
-curl -# -sL -O $dl_url   | tar -zxf ${app_file_name}${app_file_ext} -C ${app_path}
-
-mv /usr/local/${app_name}/${app_service} /usr/lib/systemd/system
-if ! ls /usr/bin/${app_bin} >/dev/null 2>&1; then
-  ln -s /usr/local/${app_name}/${app_bin} /usr/bin/${app_bin}
-fi
-
-if ! ls /etc/systemd/system/multi-user.target.wants/${app_service} >/dev/null 2>&1; then
-  ln -s /usr/lib/systemd/system/${app_service} /etc/systemd/system/multi-user.target.wants/${app_service}
-fi
-systemctl daemon-reload
+echo "done"
