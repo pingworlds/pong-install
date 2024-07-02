55934156f4644a52179ea5c1d3cd653e83653de5
pingworlds <99244689+pingworlds@users.noreply.github.com>
2024-07-02 13:02:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,7 +83,9 @@ download_file() {
 	rm -rf ${app_file_name}${app_file_ext}
 
 	chmod -R 700 /usr/local/${app_name}/
-	ln -sf  /usr/local/${app_name}/${app_name} /usr/bin/${app_name}    
+        if ! ls /usr/bin/${app_name} >/dev/null 2>&1; then
+	  ln -sf  /usr/local/${app_name}/${app_name} /usr/bin/${app_name}   
+        fi
 	 
 }
 
